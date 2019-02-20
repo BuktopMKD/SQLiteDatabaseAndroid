85cb95ecfe4258e3903073d9eb119f0479676b3f
Viktor Jovanovski <BuktopMKD@gmail.com>
2019-02-20 11:44:09 +0100

    Insert in to Database - Implement

--- app/src/main/java/com/denofdevelopers/blogdbmanager_parttwo/DataBaseHelper.java
+++ app/src/main/java/com/denofdevelopers/blogdbmanager_parttwo/DataBaseHelper.java
@@ -1,15 +1,15 @@
 package com.denofdevelopers.blogdbmanager_parttwo;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 public class DataBaseHelper extends SQLiteOpenHelper {
 
-    // DB information
-    public static final String DB_NAME = "Person.db";              // Camel case don`t apply & location of the file after creating is in data/data/app on device
-    public static final String STUDENT_TABLE = "student_table";    // Table name
-    public static final String COL_1_ID = "id";                    // Column name
+    public static final String DB_NAME = "Person.db";
+    public static final String STUDENT_TABLE = "student_table";
+    public static final String COL_1_ID = "id";
     public static final String COL_2_NAME = "name";
     public static final String COL_3_LAST_NAME = "last_name";
     public static final String COL_4_DATE_OF_BIRTH = "date_of_birth";
@@ -17,19 +17,11 @@ public class DataBaseHelper extends SQLiteOpenHelper {
     public static final int VERSION = 1;
 
     public DataBaseHelper(Context context) {
-        // Here the DB is created
-        // Whenever this constructor is called, database is created
-        // Create the database with our own arguments
         super(context, DB_NAME, null, VERSION);
-
-        // This below is just for checking on first run to see that the db is created
-        SQLiteDatabase sqLiteDatabase = this.getWritableDatabase();
     }
 
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
-        // Command that executes any quarry
-        // Creating a table with given columns
         sqLiteDatabase.execSQL("CREATE TABLE "
                 + STUDENT_TABLE + " ("
                 + COL_1_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
@@ -44,5 +36,40 @@ public class DataBaseHelper extends SQLiteOpenHelper {
     public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
         sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + STUDENT_TABLE);
         onCreate(sqLiteDatabase);
+    }
+
+    // Insert Data
+    public boolean insertData(String name, String lastName, String dateOfBirth, String gender) {
+
+        // Creating the DB
+        SQLiteDatabase sqLiteDatabase = this.getWritableDatabase();
+
+        // Object for inserting data in to DB
+        ContentValues contentValues = new ContentValues();
+
+        // .put for inserting preparing data for inserting in to DB
+        // arg1 for column name
+        // arg2 for actual data
+        contentValues.put(COL_2_NAME, name);
+        contentValues.put(COL_3_LAST_NAME, lastName);
+        contentValues.put(COL_4_DATE_OF_BIRTH, dateOfBirth);
+        contentValues.put(COL_5_GENDER, gender);
+
+        // Actual inserting the data in to Student.db
+        // arg1 table name
+        // arg2 nullHackStuff
+        // arg3 content values
+        long insertResult = sqLiteDatabase.insert(STUDENT_TABLE, null, contentValues);
+
+        // .insert method returns row id
+        // if there is error in inserting it will return -1
+        if (insertResult != -1) {
+            return true;
+        } else {
+            return false;
+        }
+
+        // Simplified return (same as above)
+        // return insertResult != -1;
     }
 }
--- app/src/main/java/com/denofdevelopers/blogdbmanager_parttwo/MainActivity.java
+++ app/src/main/java/com/denofdevelopers/blogdbmanager_parttwo/MainActivity.java
@@ -2,15 +2,48 @@ package com.denofdevelopers.blogdbmanager_parttwo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
 
     DataBaseHelper dataBase;
+    @BindView(R.id.editName)
+    EditText editName;
+    @BindView(R.id.editLastName)
+    EditText editLastName;
+    @BindView(R.id.editDateOfBirth)
+    EditText editDateOfBirth;
+    @BindView(R.id.editGender)
+    EditText editGender;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
         dataBase = new DataBaseHelper(this);
+    }
+
+    @OnClick(R.id.addButton)
+    public void onAddClick() {
+        if (!TextUtils.isEmpty(editName.getText().toString())
+                && !TextUtils.isEmpty(editLastName.getText().toString())
+                && !TextUtils.isEmpty(editDateOfBirth.getText().toString())
+                && !TextUtils.isEmpty(editGender.getText().toString())) {
+            boolean isDataInserted = dataBase.insertData(editName.getText().toString(), editLastName.getText().toString(), editDateOfBirth.getText().toString(), editGender.getText().toString());
+            if (isDataInserted) {
+                Toast.makeText(this, "Data inserted to database", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(this, "Data NOT inserted!", Toast.LENGTH_SHORT).show();
+            }
+        } else {
+            Toast.makeText(this, "Please fill all fields", Toast.LENGTH_SHORT).show();
+        }
     }
 }
