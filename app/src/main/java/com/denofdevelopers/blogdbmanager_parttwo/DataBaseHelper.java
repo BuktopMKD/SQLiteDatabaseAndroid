761f941d33437539b01390b4f6f586af23b76792
Viktor Jovanovski <BuktopMKD@gmail.com>
2019-03-19 13:41:35 +0100

    Refactor

--- app/src/main/java/com/denofdevelopers/blogdbmanager_parttwo/DataBaseHelper.java
+++ app/src/main/java/com/denofdevelopers/blogdbmanager_parttwo/DataBaseHelper.java
@@ -38,38 +38,14 @@ public class DataBaseHelper extends SQLiteOpenHelper {
         onCreate(sqLiteDatabase);
     }
 
-    // Insert Data
     public boolean insertData(String name, String lastName, String dateOfBirth, String gender) {
-
-        // Creating the DB
         SQLiteDatabase sqLiteDatabase = this.getWritableDatabase();
-
-        // Object for inserting data in to DB
         ContentValues contentValues = new ContentValues();
-
-        // .put for inserting preparing data for inserting in to DB
-        // arg1 for column name
-        // arg2 for actual data
         contentValues.put(COL_2_NAME, name);
         contentValues.put(COL_3_LAST_NAME, lastName);
         contentValues.put(COL_4_DATE_OF_BIRTH, dateOfBirth);
         contentValues.put(COL_5_GENDER, gender);
-
-        // Actual inserting the data in to Student.db
-        // arg1 table name
-        // arg2 nullHackStuff
-        // arg3 content values
         long insertResult = sqLiteDatabase.insert(STUDENT_TABLE, null, contentValues);
-
-        // .insert method returns row id
-        // if there is error in inserting it will return -1
-        if (insertResult != -1) {
-            return true;
-        } else {
-            return false;
-        }
-
-        // Simplified return (same as above)
-        // return insertResult != -1;
+        return insertResult != -1;
     }
 }
